e16f1d5ef9c07a802be84609e73b5af835627c1a
Raj Sri Gulshan <rajgulshan20022000@gmail.com>
2025-06-22 01:19:55 +0530

    contextApiDemo_Done

--- contextApiDemo/src/App.jsx
+++ contextApiDemo/src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Profile from './components/Profile'
+import Login from './components/Login'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
   return (
-    <>
-     <h1>Hello</h1>
-    </>
+    <UserContextProvider>
+     <Login />
+     <Profile />
+    </UserContextProvider>
   )
 }
 
